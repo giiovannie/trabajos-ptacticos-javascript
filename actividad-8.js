eb16f1ef92eba7c6d596bd2e06cd0dce79d35fdb
gomez ivan <ivanemmanuelgomez2017@gmail.com>
2026-05-23 16:55:30 -0300

    feat(actividad-8.js)se resolvieron las actividades del punto 8

---
+++ actividad-8.js
@@ -0,0 +1,17 @@
+// Actividad 8. Arreglo de notas
+// Consigna: Creá un arreglo con 5 notas. Calculá el promedio usando un bucle y mostrá si el
+// alumno aprobó o no.
+// Objetivo: Practicar arreglos y bucles en conjunto.
+// Pista: Recorre el arreglo sumando cada nota con un bucle for, luego se divide por notas.length
+// para obtener el promedio.
+
+let notas = [10,7,9,6,7]
+let promedio = 0;
+
+for(let num of notas){
+    promedio += prom
+}
+
+promedio /= notas.length
+
+document.write(`El promedio de las notas es: ${promedio}`)
